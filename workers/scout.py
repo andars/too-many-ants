6242c87992a3096c103da308e0a90481ddbdfbc8
andars <afoote97@gmail.com>
2015-02-07 17:17:41 -0800

    add index and assignment to scout

--- workers/scout.py
+++ workers/scout.py
@@ -1,9 +1,16 @@
 class Scout():
-    def __init__(self, group, target):
+    def __init__(self, group, assignment):
         self.group = group
-        self.target = target
-
-    def searchFile(self, filePath):
+        self.index = 0
+        self.assignment = assignment
+        
+    def search_file(self, filePath):
         fileHandler = open(filePath, "r")
         fileData = fileHandler.read()
         fileHandler.close()
+
+    def get_index(self):
+        return self.index
+
+    def get_assignment(self):
+        return self.assignment
