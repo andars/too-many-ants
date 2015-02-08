d5f53f3f3793b80677fe3b412df7191fc101a9b9
Ritwik Dutta <ritzymail@gmail.com>
2015-02-07 17:40:29 -0800

    Fixed

--- search.py
+++ search.py
@@ -3,25 +3,25 @@ from os import listdir
 empty = None
 
 class WorkerController():
-	def __init__(self):
-		self.workers = []
+    def __init__(self):
+        self.workers = []
 
-	def read_worker_messages(self):
-		worker_messages = {}
-		for worker in self.workers:
-			if worker_messages[worker.group] is empty:
-				worker_messages[worker.group] = []
-			worker_messages[worker.group].append(worker.messages)
-			worker.clear_essage
+    def read_worker_messages(self):
+        worker_messages = {}
+        for worker in self.workers:
+            if worker_messages[worker.group] is empty:
+                worker_messages[worker.group] = []
+            worker_messages[worker.group].append(worker.messages)
+            worker.clear_essage
 
-		return worker_messages
+        return worker_messages
 
 
-	def get_document_list(self, documentPath):
-		documents = listdir(documentPath)
-		num_docs = len(documents)
-		worker_loads = [documents[i:i+3] for i in range(0,len(documents),3)]
-		return worker_loads
+    def get_document_list(self, documentPath):
+        documents = listdir(documentPath)
+        num_docs = len(documents)
+        worker_loads = [documents[i:i+3] for i in range(0,len(documents),3)]
+        return worker_loads
 
 search_worker_controller = WorkerController()
 
